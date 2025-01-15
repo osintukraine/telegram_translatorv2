58778311f7ab64e145d76f829704de00138f1b0a
Ben Borges <me@benborges.xyz>
2025-01-15 08:07:43 +0100

    better logging

--- src/listener-db.py
+++ src/listener-db.py
@@ -1,6 +1,6 @@
 import sqlite3
 from telethon import TelegramClient, events
-from telethon.tl.types import InputChannel
+from telethon.tl.types import InputChannel, MessageMediaWebPage
 from dotenv import load_dotenv
 import os
 import deepl
@@ -9,35 +9,33 @@ import yaml
 import html
 import re
 
-# Load environment variables from .env
+# Load environment variables
 load_dotenv()
 
 DEEPL_AUTH_KEY = os.getenv("DEEPL_AUTH_KEY")
 translator = deepl.Translator(DEEPL_AUTH_KEY)
 
-# Set up logging
+# Logging setup
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
 logging.getLogger('telethon').setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Additional loggers for debugging
+# Debug loggers
 seq_matcher_logger = logging.getLogger('seq_matcher')
 seq_matcher_logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('seq_matcher_logs.log')
-seq_matcher_logger.addHandler(file_handler)
+seq_matcher_logger.addHandler(logging.FileHandler('seq_matcher_logs.log'))
 
 store_msg_logger = logging.getLogger('store_message')
 store_msg_logger.setLevel(logging.DEBUG)
-store_msg_file_handler = logging.FileHandler('store_message_logs.log')
-store_msg_logger.addHandler(store_msg_file_handler)
+store_msg_logger.addHandler(logging.FileHandler('store_message_logs.log'))
 
-# Load credentials and channels from config files
+# Load config files
 with open('config.yml', 'rb') as f:
     config = yaml.safe_load(f)
 with open('channels.yml', 'rb') as f:
     channels = yaml.safe_load(f)
 
-# Initialize Telegram client
+# Telegram client setup
 client = TelegramClient(config["session_name"], config["api_id"], config["api_hash"])
 client.start()
 print('[Telethon] Client is listening...')
@@ -63,7 +61,7 @@ rus_photos_channel = channels['output_channel_ids'][3]
 ukr_videos_channel = channels['output_channel_ids'][4]
 rus_videos_channel = channels['output_channel_ids'][5]
 
-# SQLite Database setup
+# SQLite database setup
 conn = sqlite3.connect('messages.db')
 cursor = conn.cursor()
 cursor.execute('''
@@ -78,20 +76,16 @@ CREATE TABLE IF NOT EXISTS messages (
 ''')
 conn.commit()
 
-# Check if message has already been processed
 def is_message_seen(channel_id, message_id):
     cursor.execute("SELECT * FROM messages WHERE channel_id = ? AND message_id = ?", (channel_id, message_id))
-    result = cursor.fetchone()
-    return bool(result)
+    return bool(cursor.fetchone())
 
-# Store processed message
 def store_message(channel_id, message_id, content, link, date):
     cursor.execute("INSERT INTO messages (channel_id, message_id, content, link, date) VALUES (?, ?, ?, ?, ?)",
                    (channel_id, message_id, content, link, date))
     conn.commit()
-    store_msg_logger.debug(f"Stored message from channel: {channel_id}, message_id: {message_id}, link: {link}")
+    store_msg_logger.debug(f"Stored message: Channel {channel_id}, Message ID {message_id}, Link {link}")
 
-# Prefabricate the message to send
 def create_message(chat_name, untranslated_msg, translation, link):
     border = '<br>'
     if translation:
@@ -102,26 +96,17 @@ def create_message(chat_name, untranslated_msg, translation, link):
             f'<p>[TRANSLATED MESSAGE]\n'
             f'{html.escape(translation)}\n\n</p>'
             f'<p>{border}\n'
-            f'{link} ↩</p></p>')
+            f'{link} ↩</p></p>'
+        )
     else:
         message = (
             f'<p>{border}\n'
             f'<b>{html.escape(chat_name)}</b>\n'
             f'{border}\n\n'
-            f'{link} ↩</p>')
-
-    # Trim message if it exceeds Telegram limits
+            f'{link} ↩</p>'
+        )
     if len(message) >= 3980:
-        formatting_chars_len = len(
-            f'<p><p>{border}\n' + 
-            f'<b>{html.escape(chat_name)}</b>\n' + 
-            f'{border}\n\n</p>' + 
-            f'<p>[TRANSLATED MESSAGE]\n' + 
-            f'\n\n</p>' + 
-            f'<p>{border}\n' + 
-            f'{link} ↩</p></p>')
-        desired_msg_len = 3980 - formatting_chars_len - 3
-        translation = f'{translation[0:desired_msg_len]}...'
+        translation = f'{translation[:3980]}...'
         message = (
             f'<p><p>{border}\n'
             f'<b>{html.escape(chat_name)}</b>\n'
@@ -129,11 +114,10 @@ def create_message(chat_name, untranslated_msg, translation, link):
             f'<p>[TRANSLATED MESSAGE]\n'
             f'{html.escape(translation)}\n\n</p>'
             f'<p>{border}\n'
-            f'{link} ↩</p></p>')
-
+            f'{link} ↩</p></p>'
+        )
     return message
 
-# Event handlers
 @client.on(events.NewMessage(chats=ukraine_channels_entities))
 async def handle_ukraine_messages(event):
     chat = await event.get_chat()
@@ -142,29 +126,27 @@ async def handle_ukraine_messages(event):
     message_id = event.id
     link = f't.me/{chat.username or f"c/{chat.id}"}/{message_id}'
 
-    # Log the incoming message details
-    logger.info(f"Received message from Ukraine channel: {chat_name}, ID: {message_id}")
+    logger.info(f"Received Ukraine message: Channel {chat.id}, Message ID {message_id}")
 
     untranslated_msg = event.message.message or ""
     if event.message.media:
         if isinstance(event.message.media, MessageMediaWebPage):
-            logger.info(f"Handling a web page message from {chat_name}, ID: {message_id}")
-            content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
-            message = create_message(chat_name, untranslated_msg, content, link)
-            await client.send_message(ukraine_news_channel, message, parse_mode='html', link_preview=True)
+            logger.warning(f"Unsupported media: Channel {chat.id}, Message ID {message_id}, Link {link}")
         elif event.message.photo:
             content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
             message = create_message(chat_name, untranslated_msg, content, link)
             await client.send_message(ukraine_news_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
             await client.send_message(ukr_photos_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
-        elif event.message.document and re.search('video', event.message.media.document.mime_type):
-            content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
-            message = create_message(chat_name, untranslated_msg, content, link)
-            await client.send_message(ukr_videos_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
-        else:
-            content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
-            message = create_message(chat_name, untranslated_msg, content, link)
-            await client.send_message(ukraine_news_channel, message, parse_mode='html', link_preview=False)
+        elif event.message.document:
+            mime_type = event.message.media.document.mime_type
+            if re.search('video', mime_type):
+                content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
+                message = create_message(chat_name, untranslated_msg, content, link)
+                await client.send_message(ukr_videos_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
+            else:
+                content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
+                message = create_message(chat_name, untranslated_msg, content, link)
+                await client.send_message(ukraine_news_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
     else:
         content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
         message = create_message(chat_name, untranslated_msg, content, link)
@@ -180,29 +162,27 @@ async def handle_russia_messages(event):
     message_id = event.id
     link = f't.me/{chat.username or f"c/{chat.id}"}/{message_id}'
 
-    # Log the incoming message details
-    logger.info(f"Received message from Russia channel: {chat_name}, ID: {message_id}")
+    logger.info(f"Received Russia message: Channel {chat.id}, Message ID {message_id}")
 
     untranslated_msg = event.message.message or ""
     if event.message.media:
         if isinstance(event.message.media, MessageMediaWebPage):
-            logger.info(f"Handling a web page message from {chat_name}, ID: {message_id}")
-            content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
-            message = create_message(chat_name, untranslated_msg, content, link)
-            await client.send_message(russia_news_channel, message, parse_mode='html', link_preview=True)
+            logger.warning(f"Unsupported media: Channel {chat.id}, Message ID {message_id}, Link {link}")
         elif event.message.photo:
             content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
             message = create_message(chat_name, untranslated_msg, content, link)
             await client.send_message(russia_news_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
             await client.send_message(rus_photos_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
-        elif event.message.document and re.search('video', event.message.media.document.mime_type):
-            content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
-            message = create_message(chat_name, untranslated_msg, content, link)
-            await client.send_message(rus_videos_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
-        else:
-            content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
-            message = create_message(chat_name, untranslated_msg, content, link)
-            await client.send_message(russia_news_channel, message, parse_mode='html', link_preview=False)
+        elif event.message.document:
+            mime_type = event.message.media.document.mime_type
+            if re.search('video', mime_type):
+                content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
+                message = create_message(chat_name, untranslated_msg, content, link)
+                await client.send_message(rus_videos_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
+            else:
+                content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
+                message = create_message(chat_name, untranslated_msg, content, link)
+                await client.send_message(russia_news_channel, message, parse_mode='html', file=event.message.media, link_preview=False)
     else:
         content = translator.translate_text(untranslated_msg, target_lang="EN-US").text if untranslated_msg else ""
         message = create_message(chat_name, untranslated_msg, content, link)
@@ -210,5 +190,4 @@ async def handle_russia_messages(event):
 
     store_message(chat.id, message_id, untranslated_msg, link, date)
 
-# Run client
 client.run_until_disconnected()
